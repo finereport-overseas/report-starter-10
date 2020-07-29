6f0b8d0ea1fe0c6fd9b4a7048d5d67324d16a831
Joe.Jiang <joe.jiang@fanruan.com>
2020-07-29 11:51:54 +0800

    JS & CSS include

---
+++ plugin-JSCSS-include/src/main/java/com/fr/plugin/jscss/include/demo/FileDef.java
@@ -0,0 +1,51 @@
+package com.fr.plugin.jscss.include.demo;
+
+import com.fr.plugin.transform.ExecuteFunctionRecord;
+import com.fr.plugin.transform.FunctionRecorder;
+import com.fr.web.struct.Component;
+import com.fr.web.struct.Filter;
+import com.fr.web.struct.browser.RequestClient;
+import com.fr.web.struct.category.ScriptPath;
+import com.fr.web.struct.category.StylePath;
+
+/**
+ * @author Joe
+ * Created by Joe on 7/29/2020
+ */
+@FunctionRecorder
+public class FileDef extends Component {
+    public static final FileDef KEY = new FileDef();
+    private FileDef(){}
+    /**
+     * Provide the path of the JS file we want to include.
+     * @param client A client object
+     * @return The path of the JS file
+     */
+    public ScriptPath script(RequestClient client) {
+        // Return ScriptPath.Empty if no need.
+        return ScriptPath.build("com/fr/plugin/jscss/include/demo/web/demo.js");
+    }
+    /**
+     * Provide the path of the CSS file we want to include.
+     * @param client A client object
+     * @return The path of the CSS file
+     */
+    public StylePath style(RequestClient client) {
+        // Return StylePath.Empty if no need.
+        return StylePath.build("com/fr/plugin/jscss/include/demo/web/demo.css");
+    }
+
+    /**
+     * Decide whether to load this Atom by a filter
+     * @return A filter
+     */
+    @ExecuteFunctionRecord
+    public Filter filter(){
+        return new Filter() {
+            @Override
+            public boolean accept() {
+                return true;
+            }
+        };
+    }
+}
---
+++ plugin-JSCSS-include/src/main/resources/com/fr/plugin/jscss/include/demo/web/demo.js
@@ -0,0 +1,3 @@
+;(function() {
+    $("<div/>").addClass("plugin-jscss-include-demo").html("Hello World!").appendTo("body");
+})();
